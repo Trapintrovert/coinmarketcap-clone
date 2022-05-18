42fff1b243fdeb3863d323f7822038476713b424
Adedamola Adeniyi <mcneo062@gmail.com>
2022-05-18 17:38:31 +0100

    Header done

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import Search from '../assets/svg/search'
+import { ConnectButton } from 'web3uikit'
 
 const styles = {
   navLink: `text-white flex mx-[10px]`,
@@ -56,7 +57,7 @@ const Header = () => {
         </nav>
 
         <div className="flex items-center">
-          { /* <ConnectButton/> */}
+          <ConnectButton/>
           <div className={styles.inputContainer}>
             <Search/>
             <input className={styles.input} placeholder="Search"/>
