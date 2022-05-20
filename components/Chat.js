9ba1f779c08354400448f5d2cb482f6cf4a07ffd
Adedamola Adeniyi <mcneo062@gmail.com>
2022-05-20 03:36:04 +0100

    Button added  to chat

--- components/Chat.js
+++ components/Chat.js
@@ -3,6 +3,7 @@ import ChevronDown from '../assets/svg/chevronDown'
 import ChevronUp from '../assets/svg/chevronUp'
 import shiba from '../assets/shiba.png'
 import Image from 'next/image'
+import Button from './Button'
 
 const styles = {
   bullishLabel: `flex cursor-pointer active:bg-green-600 items-center text text-green-600 border border-green-600 h-min px-2 rounded-lg`,
@@ -21,8 +22,86 @@ const styles = {
 
 
 const Chat = () => {
+  const [message, setMessage] = useState('')
+  const [bullishValue, setBullishValue] = useState(true)
+
+  const sendMessage = () => {
+    console.log(message)
+  }
+
   return (
-    <div>Ckat</div>
+    <>
+      <div className={styles.chat}>
+        <div className={styles.flexBetween}>
+          <p className={styles.boldText}>Live Shiba Inu Chat</p>
+          <p className='text-[#6188FF]'>See more</p>
+        </div>
+
+        <br/>
+
+        <div className={styles.chatContainer}>
+          <div className={styles.flexBetween}>
+            <div className={styles.flexCenter}>
+              <div>
+                <Image src={shiba} width={70} height={70}/>
+              </div>
+              <div className='text-left mr-10'>
+                <b>Lance</b>
+                <p className='text-grey-400'>@trapintrovert</p>
+              </div>
+            </div>
+
+            <div className={styles.flexCenter}>
+              <div 
+                className={!bullishValue ? styles.bullishLabel : styles.activeBullishLabel}
+                onClick={() => setBullishValue(true)}
+              >
+                <ChevronUp fill='#17C784'/>
+                <small className='ml-1'>Bullish</small>
+              </div>
+              &nbsp; &nbsp;
+              <div
+                className={bullishValue ? styles.bearishLabel : styles.activeBearishLabel}
+                onClick={() => setBullishValue(false)}
+              >
+                <ChevronDown fill='#a52b2b'/>
+                <small className='ml-1'>Bearish</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='flex'>
+        <div className='flex items-center text text-green-600'>
+          <ChevronUp fill='#22bc64' />
+          <small className='ml-1'>Bullish</small>
+        </div>
+        &nbsp; &nbsp;
+        <div className='flex items-center text text-red-500'>
+          <ChevronDown fill='#a52b2b' />
+          <small className='ml-1'>Bearish</small>
+        </div>
+      </div>
+
+      <input
+        className={styles.input}
+        placeholder="What's happening on BTC?"
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+      />
+
+      <div className={styles.postButtonContainer}>
+        <Button label='Post' onPress={sendMessage} />
+      </div>
+
+      {
+        /*
+          format the message
+          <ChatCard/>
+        */
+      }
+    </>
   )
 }
 
